fec54abb45424554459372812c03b4cc187900fd
Viktor Sarge <viktor.sarge@regionhalland.se>
2018-04-23 14:11:50 +0200

    Updating meetings once a minute

--- js/connector.js
+++ js/connector.js
@@ -1,16 +1,14 @@
+var reportUrl = "https://script.googleusercontent.com/macros/echo?user_content_key=sLZ5_UXKWkknzBykaAwONIVZftR6ab-COCbYHdoGmdHbgNVq-JETbhytClvm_Ia0uAioLpk1EenYdsLc3lmOFcdOwHi1gy8Em5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnPkmXcN78X7GNaRmWHhfpJJZ9UDB9jR0TZHzQmGtMoqSG5FSnj2YaWegAcovC2ObEWqreFRoDX_5&lib=MQv25UDfSmrECOrn40GdUGXfCTEkP8scq";
 
 function addMinutesClass (time) {
     time = time.toString();
     return time.substring(0,2) + "<span class='minutes'>" + time.substring(2,4) + "</span>";
 }
 
-$(document).ready(function () {
-    var reportUrl = "https://script.googleusercontent.com/macros/echo?user_content_key=sLZ5_UXKWkknzBykaAwONIVZftR6ab-COCbYHdoGmdHbgNVq-JETbhytClvm_Ia0uAioLpk1EenYdsLc3lmOFcdOwHi1gy8Em5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnPkmXcN78X7GNaRmWHhfpJJZ9UDB9jR0TZHzQmGtMoqSG5FSnj2YaWegAcovC2ObEWqreFRoDX_5&lib=MQv25UDfSmrECOrn40GdUGXfCTEkP8scq";
-    $.getJSON(reportUrl, function (result) {
-        console.log(result);
-        var snippet = "<ul>";
+function createMeetingsHTML (JSONdata) {
+    var snippet = "<ul>";
         var meetingrow = "";
-        $.each(result, function(index, row){
+        $.each(JSONdata, function(index, row){
             row.Start = addMinutesClass(row.Start);
             row.Slut = addMinutesClass(row.Slut);
             meetingrow = "<li class='meeting'><div><span class='time'>" + row.Start + " - " + row.Slut + "</span>";
@@ -19,6 +17,22 @@ $(document).ready(function () {
             meetingrow = "";
         });
         snippet += "</ul>";
-        $("#meetingsContainer").append(snippet);
+    return snippet;
+}
+
+function renderMeetings (htmlSnippet) {
+    $("#meetingsContainer").html(htmlSnippet);
+}
+
+$(document).ready(function () {
+    $.getJSON(reportUrl, function (result) {
+        var snippet = createMeetingsHTML(result);
+        renderMeetings(snippet);
+        setInterval(function (jsondata) {
+            $.getJSON(reportUrl, function (result) {
+                var snippet = createMeetingsHTML(result);
+                renderMeetings(snippet);    
+            });
+        }, 60000, [result]);
     });
 });
