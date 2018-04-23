d600493bf8d675b168dae30a04f65d8ef3f653ff
Viktor Sarge <viktor.sarge@regionhalland.se>
2018-04-23 14:23:55 +0200

    linting

--- js/connector.js
+++ js/connector.js
@@ -1,38 +1,39 @@
-var reportUrl = "https://script.googleusercontent.com/macros/echo?user_content_key=sLZ5_UXKWkknzBykaAwONIVZftR6ab-COCbYHdoGmdHbgNVq-JETbhytClvm_Ia0uAioLpk1EenYdsLc3lmOFcdOwHi1gy8Em5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnPkmXcN78X7GNaRmWHhfpJJZ9UDB9jR0TZHzQmGtMoqSG5FSnj2YaWegAcovC2ObEWqreFRoDX_5&lib=MQv25UDfSmrECOrn40GdUGXfCTEkP8scq";
+"use strict";
 
-function addMinutesClass (time) {
+function addMinutesClass(time) {
     time = time.toString();
-    return time.substring(0,2) + "<span class='minutes'>" + time.substring(2,4) + "</span>";
+    return time.substring(0, 2) + "<span class='minutes'>" + time.substring(2, 4) + "</span>";
 }
 
-function createMeetingsHTML (JSONdata) {
+function createMeetingsHTML(JSONdata) {
     var snippet = "<ul>";
-        var meetingrow = "";
-        $.each(JSONdata, function(index, row){
-            row.Start = addMinutesClass(row.Start);
-            row.Slut = addMinutesClass(row.Slut);
-            meetingrow = "<li class='meeting'><div><span class='time'>" + row.Start + " - " + row.Slut + "</span>";
-            meetingrow += "<span class='desc'>" + row.Beskrivning + "</span>"+ "<span class='location'>" + row.Plats + "</span></div></li>";
-            snippet += meetingrow;
-            meetingrow = "";
-        });
-        snippet += "</ul>";
+    var meetingrow = "";
+    $.each(JSONdata, function (index, row) {
+        row.Start = addMinutesClass(row.Start);
+        row.Slut = addMinutesClass(row.Slut);
+        meetingrow = "<li class='meeting'><div><span class='time'>" + row.Start + " - " + row.Slut + "</span>";
+        meetingrow += "<span class='desc'>" + row.Beskrivning + "</span>" + "<span class='location'>" + row.Plats + "</span></div></li>";
+        snippet += meetingrow;
+        meetingrow = "";
+    });
+    snippet += "</ul>";
     return snippet;
 }
 
-function renderMeetings (htmlSnippet) {
+function renderMeetings(htmlSnippet) {
     $("#meetingsContainer").html(htmlSnippet);
 }
 
 $(document).ready(function () {
+    var reportUrl = "https://script.googleusercontent.com/macros/echo?user_content_key=sLZ5_UXKWkknzBykaAwONIVZftR6ab-COCbYHdoGmdHbgNVq-JETbhytClvm_Ia0uAioLpk1EenYdsLc3lmOFcdOwHi1gy8Em5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnPkmXcN78X7GNaRmWHhfpJJZ9UDB9jR0TZHzQmGtMoqSG5FSnj2YaWegAcovC2ObEWqreFRoDX_5&lib=MQv25UDfSmrECOrn40GdUGXfCTEkP8scq";
     $.getJSON(reportUrl, function (result) {
         var snippet = createMeetingsHTML(result);
         renderMeetings(snippet);
-        setInterval(function (jsondata) {
+        setInterval(function () {
             $.getJSON(reportUrl, function (result) {
-                var snippet = createMeetingsHTML(result);
-                renderMeetings(snippet);    
+                snippet = createMeetingsHTML(result);
+                renderMeetings(snippet);
             });
-        }, 60000, [result]);
+        }, 60000);
     });
 });
