11d24dcbb291d5bbf45c6ce9150185023cd88318
Viktor Sarge <viktor.sarge@regionhalland.se>
2018-04-30 11:39:55 +0200

    Refactoring

--- js/connector.js
+++ js/connector.js
@@ -60,7 +60,7 @@ function renderMeetings(htmlSnippet) {
     $("#meetingsContainer").html(htmlSnippet);
 }
 
-$(document).ready(function () {
+function updatePage() {
     var reportUrl = "https://script.googleusercontent.com/macros/echo?user_content_key=sLZ5_UXKWkknzBykaAwONIVZftR6ab-COCbYHdoGmdHbgNVq-JETbhytClvm_Ia0uAioLpk1EenYdsLc3lmOFcdOwHi1gy8Em5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnPkmXcN78X7GNaRmWHhfpJJZ9UDB9jR0TZHzQmGtMoqSG5FSnj2YaWegAcovC2ObEWqreFRoDX_5&lib=MQv25UDfSmrECOrn40GdUGXfCTEkP8scq";
     $.getJSON(reportUrl, function (result) {
         if (getParameterByName("hidePreviousMeetings") === "true") {
@@ -68,14 +68,12 @@ $(document).ready(function () {
         }
         var snippet = createMeetingsHTML(result);
         renderMeetings(snippet);
-        setInterval(function () {
-            $.getJSON(reportUrl, function (result) {
-                if (getParameterByName("hidePreviousMeetings") === "true") {
-                    result = removeOldMeeting(result);
-                }
-                snippet = createMeetingsHTML(result);
-                renderMeetings(snippet);
-            });
-        }, 3000);
     });
+}
+
+$(document).ready(function () {
+    updatePage();
+    setInterval(function () {
+        updatePage();
+    }, 3000);
 });
