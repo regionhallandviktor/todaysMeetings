1dc4272ce9d19f2ae24d51f032baacb716dac1bb
Viktor Sarge <viktor.sarge@regionhalland.se>
2018-04-30 11:02:07 +0200

    Option to hide old meetings

--- js/connector.js
+++ js/connector.js
@@ -1,5 +1,39 @@
 "use strict";
 
+function removeOldMeeting(indata) {
+    var result = [];
+    var currentTime = new Date();
+    var currentHours = currentTime.getHours();
+    var currentMinutes = currentTime.getMinutes();
+    console.log(currentHours, currentMinutes);
+    var time = "";
+    $.each(indata, function (index, row){
+        time = row.Slut.toString();
+        var minutes = time.substring(time.length - 2);
+        var hours = time.substring(0, time.length - 2);
+        if (Number(hours) > Number(currentHours) || Number(minutes) > Number(currentMinutes)) {
+            result.push(row);
+        }
+    });
+    return result;
+}
+
+function getParameterByName(name, url) {
+    if (!url) { 
+        url = window.location.href;
+    }
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)");
+    var results = regex.exec(url);
+    if (!results) {
+        return null;
+    }
+    if (!results[2]) {
+        return "";
+    }
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
 function addMinutesClass(time) {
     time = time.toString();
     var minutes = time.substring(time.length - 2);
@@ -29,6 +63,9 @@ function renderMeetings(htmlSnippet) {
 $(document).ready(function () {
     var reportUrl = "https://script.googleusercontent.com/macros/echo?user_content_key=sLZ5_UXKWkknzBykaAwONIVZftR6ab-COCbYHdoGmdHbgNVq-JETbhytClvm_Ia0uAioLpk1EenYdsLc3lmOFcdOwHi1gy8Em5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnPkmXcN78X7GNaRmWHhfpJJZ9UDB9jR0TZHzQmGtMoqSG5FSnj2YaWegAcovC2ObEWqreFRoDX_5&lib=MQv25UDfSmrECOrn40GdUGXfCTEkP8scq";
     $.getJSON(reportUrl, function (result) {
+        if (getParameterByName("hidePreviousMeetings") === "true") {
+            result = removeOldMeeting(result);
+        }
         var snippet = createMeetingsHTML(result);
         renderMeetings(snippet);
         setInterval(function () {
