1dc292c4d1552373985980624be2bf414a61cec0
Viktor Sarge <viktor.sarge@regionhalland.se>
2018-05-03 08:48:38 +0200

    Clarified error message + removed manual trigger of error

--- js/connector.js
+++ js/connector.js
@@ -65,7 +65,7 @@ function renderMeetings(htmlSnippet) {
 }
 
 function updatePage(refreshTimer) {
-    var reportUrl = "https://script.googleusercontent.comXXXMXMXMXM/macros/echo?user_content_key=sLZ5_UXKWkknzBykaAwONIVZftR6ab-COCbYHdoGmdHbgNVq-JETbhytClvm_Ia0uAioLpk1EenYdsLc3lmOFcdOwHi1gy8Em5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnPkmXcN78X7GNaRmWHhfpJJZ9UDB9jR0TZHzQmGtMoqSG5FSnj2YaWegAcovC2ObEWqreFRoDX_5&lib=MQv25UDfSmrECOrn40GdUGXfCTEkP8scq";
+    var reportUrl = "https://script.googleusercontent.com/macros/echo?user_content_key=sLZ5_UXKWkknzBykaAwONIVZftR6ab-COCbYHdoGmdHbgNVq-JETbhytClvm_Ia0uAioLpk1EenYdsLc3lmOFcdOwHi1gy8Em5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnPkmXcN78X7GNaRmWHhfpJJZ9UDB9jR0TZHzQmGtMoqSG5FSnj2YaWegAcovC2ObEWqreFRoDX_5&lib=MQv25UDfSmrECOrn40GdUGXfCTEkP8scq";
     var loadingMessage = "<ul><li class='meeting'>Möten laddas...</li></ul>";
     renderMeetings(loadingMessage);
     $.getJSON(reportUrl, function (result) {
