8caab90efb4703f310a53a5629d9779b54c79afc
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-09-23 17:51:48 +0300

    Syntax error

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@
 
 var config = {
     refreshInterval: "5000",
-    reportUrl: "https://script.googleusercontent.com/macros/echo?user_content_key=Ym_kkxRuyzEhzojEwzFW0SNwZN-Z3uCdo4ISqt_CRg5pZNRNyR-byaunFANo_QtJvz1FsG7xptXCBx7s7xpheyCZWGLKrlB8m5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnPkmXcN78X7GNaRmWHhfpJJZ9UDB9jR0TZHzQmGtMoqSG5FSnj2YaWegAcovC2ObEWqreFRoDX_5&lib=MQv25UDfSmrECOrn40GdUGXfCTEkP8scq","
+    reportUrl: "https://script.googleusercontent.com/macros/echo?user_content_key=Ym_kkxRuyzEhzojEwzFW0SNwZN-Z3uCdo4ISqt_CRg5pZNRNyR-byaunFANo_QtJvz1FsG7xptXCBx7s7xpheyCZWGLKrlB8m5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnPkmXcN78X7GNaRmWHhfpJJZ9UDB9jR0TZHzQmGtMoqSG5FSnj2YaWegAcovC2ObEWqreFRoDX_5&lib=MQv25UDfSmrECOrn40GdUGXfCTEkP8scq",
     messageloading: "Uppdatering pågår",
     messageUpdatesPaused: "Uppdatering pausad - visar morgondagens möten efter midnatt",
     messageLoadError: "Ajdå: möten kunde inte laddas. Sidan laddas om med någon minuts mellanrum, kontakta intranätservice om felet kvarstår.",
